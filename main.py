4331f5b06173b1e458d9abb26433a8ba4f69fb0d
Erick Amezcua <erickamezcua12@gmail.com>
2024-06-14 13:16:12 -0600

    Adds emp model and fix some typos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,11 +10,21 @@ app.config['SECRET_KEY'] = 'my super secret key'
 # Initialize datbase
 db = SQLAlchemy(app)
 
-# Create a model
+# Create a models
 class Dept(db.Model):
     DEPTNO = db.Column(db.Integer, primary_key=True)
-    DEPTname = db.Column(db.String(10), nullable=False)
+    DNAME = db.Column(db.String(10), nullable=False)
     LOC = db.Column(db.String(13), nullable=False)
+
+class Emp(db.Model):
+    EMPNO = db.Column(db.Integer, primary_key=True)
+    ENAME = db.Column(db.String(10), nullable=False)
+    JOB = db.Column(db.String(9))
+    MGR = db.Column(db.Integer)
+    HIREDATE = db.Column(db.DateTime)
+    SAL = db.Column(db.Double)
+    COMM = db.Column(db.Double)
+    DEPTNO = db.Column(db.Integer, db.ForeignKey("DEPT.DEPTNO"))
 
 @app.route('/')
 def home():
@@ -23,5 +34,10 @@ def home():
 def view_dept():
     depts = Dept.query.all()
     return render_template('dept.html', depts=depts)
+
+@app.route('/emp')
+def view_emp():
+    emps = Emp.query.all()
+    return render_template('emp.html', emps=emps)
 
 app.run(host='0.0.0.0', port=3000, debug=True)
