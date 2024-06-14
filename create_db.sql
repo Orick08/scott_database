23e7b833524ed03ee14989324250e5c1d322f0df
Erick Amezcua <erickamezcua12@gmail.com>
2024-06-14 11:33:52 -0600

    Adds create db sql script

---
+++ create_db.sql
@@ -0,0 +1,37 @@
+-- -----------------------------------------------------
+-- Schema SCOTT
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `SCOTT` DEFAULT CHARACTER SET utf8 ;
+USE `SCOTT` ;
+
+-- -----------------------------------------------------
+-- Table `SCOTT`.`DEPT`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SCOTT`.`DEPT` (
+  `DEPTNO` INT NOT NULL AUTO_INCREMENT,
+  `DEPTname` VARCHAR(14) NULL,
+  `LOC` VARCHAR(13) NULL,
+  PRIMARY KEY (`DEPTNO`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SCOTT`.`EMP`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SCOTT`.`EMP` (
+  `EMPNO` INT NOT NULL AUTO_INCREMENT,
+  `ENAME` VARCHAR(10) NULL,
+  `JOB` VARCHAR(8) NULL,
+  `MGR` INT NULL,
+  `HDEDATE` DATE NULL,
+  `SAL` DECIMAL NULL,
+  `COMM` DECIMAL NULL,
+  `DEPTNO` INT NOT NULL,
+  PRIMARY KEY (`EMPNO`),
+  INDEX `fk_EMP_DEPT_idx` (`DEPTNO` ASC) VISIBLE,
+  CONSTRAINT `fk_EMP_DEPT`
+    FOREIGN KEY (`DEPTNO`)
+    REFERENCES `SCOTT`.`DEPT` (`DEPTNO`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
