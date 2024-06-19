59f2e06458f80e02b18ae475a2e1b542dc10b6e1
Erick Amezcua <erickamezcua12@gmail.com>
2024-06-18 22:24:46 -0600

    Adds create new dept form

--- routes/dept.py
+++ routes/dept.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+from models.models import db
 from models.dept import Dept
 
 dept_bp = Blueprint('dept', __name__)
@@ -9,7 +13,22 @@ def view_dept():
     depts = Dept.query.all()
     return render_template('dept.html', depts=depts)
 
-@new_dept_bp.route('/dept/new')
+
+class NewDeptForm(FlaskForm):
+    name = StringField("DEPT Name", validators=[DataRequired()])
+    location = StringField("Location", validators=[DataRequired(), Length(1, 13)])
+    submit = SubmitField("Submit")
+
+@new_dept_bp.route('/dept/new', methods=['GET', 'POST'])
 def new_dept():
-    pass
+    # Create a new dept
+    form = NewDeptForm()
+    if form.validate_on_submit():
+        dept = Dept(DNAME=form.name.data,LOC=form.location.data)
+        db.session.add(dept)
+        db.session.commit()
+        return redirect('/dept')
+    
+    # Render new dept form
+    return render_template('new_dept.html', form=form)
 
